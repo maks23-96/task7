9ec426e9bd00daf3db84996a9cde97ff6f15b093
Student 5 <diskakun@gmail.com>
2017-07-28 18:35:38 +0300

    3d

--- templates/index.php
+++ templates/index.php
@@ -1,10 +1,33 @@
-<html
+<html>
 
 <head>
   <title>%TITLE%</title>
+  <link href="css/bootstrap.css" rel="stylesheet"></link>
 </head>
 <body>
-<div><h2>Contact Form</h2></div>
+<div><h2>Contact Form</h2>
+<form
+  <div>
+    <input type="text" class="form-control" placeholder="Full Name">
+  </div><br>
+<div>
+ <select class="form-control">
+  <option>Please,select</option>
+  <option>Subject 1</option>
+  <option>Subject 2</option>
+  <option>Subject 3</option>
+</select>
+</div><br>
+<div>
+  <input type="email" class="form-control" placeholder="E-mail">
+</div><br> 
+<div>
+    <textarea class="form-control" rows="3" placeholder="Your message"></textarea>
+</div><br>
+  <div>
+    <input class="btn btn-default" type="submit" value="Submit">
+ </div><br>
+</div>
 <div style="color: #FF0000; font-size: 15px;"><strong>%ERRORS%</strong></div>
 </body>
 </html>
